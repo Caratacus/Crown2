3ba97c9a5797504059ea6c53f6c4388f88703877
Caratacus <330952486@qq.com>
2019-06-19 16:50:45 +0800

    Change: Added AspectJ Proxy

--- src/main/java/com/ruoyi/CrownApplication.java
+++ src/main/java/com/ruoyi/CrownApplication.java
@@ -22,8 +22,10 @@ package com.ruoyi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class CrownApplication {
 
     public static void main(String[] args) {
