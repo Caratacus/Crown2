82e491eaabe37405633673b291c48dea3957caa7
Caratacus <330952486@qq.com>
2019-07-22 10:28:42 +0800

    Change: added typeHandler

---
+++ src/main/java/org/crown/framework/mybatisplus/typehandler/JSONArrayTypeHandler.java
@@ -0,0 +1,56 @@
+package org.crown.framework.mybatisplus.typehandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+import org.apache.ibatis.type.TypeHandler;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+
+@MappedTypes({JSONArray.class})
+@MappedJdbcTypes({JdbcType.VARCHAR})
+public class JSONArrayTypeHandler implements TypeHandler<JSONArray> {
+
+    @Override
+    public void setParameter(PreparedStatement ps, int i, JSONArray parameter, JdbcType jdbcType) throws SQLException {
+        if (Objects.nonNull(parameter)) {
+            ps.setString(i, parameter.toJSONString());
+        } else {
+            ps.setString(i, "[]");
+        }
+    }
+
+    @Override
+    public JSONArray getResult(ResultSet rs, String s) throws SQLException {
+        String columnValue = rs.getString(s);
+        return parseArray(columnValue);
+    }
+
+    @Override
+    public JSONArray getResult(ResultSet rs, int columnIndex) throws SQLException {
+        String columnValue = rs.getString(columnIndex);
+        return parseArray(columnValue);
+    }
+
+    @Override
+    public JSONArray getResult(CallableStatement cs, int i) throws SQLException {
+        String columnValue = cs.getString(i);
+        return parseArray(columnValue);
+
+    }
+
+    private JSONArray parseArray(String text) {
+        try {
+            return JSON.parseArray(text);
+        } catch (Exception ignored) {
+        }
+        return null;
+    }
+}
--- src/main/java/org/crown/framework/mybatisplus/typehandler/JSONObjectTypeHandler.java
+++ src/main/java/org/crown/framework/mybatisplus/typehandler/JSONObjectTypeHandler.java
@@ -4,13 +4,14 @@ import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Objects;
 
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedJdbcTypes;
 import org.apache.ibatis.type.MappedTypes;
-import org.crown.common.utils.JacksonUtils;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 @MappedTypes({JSONObject.class})
@@ -19,24 +20,36 @@ public class JSONObjectTypeHandler extends BaseTypeHandler<JSONObject> {
 
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, JSONObject parameter, JdbcType jdbcType) throws SQLException {
-        ps.setString(i, parameter.toJSONString());
+        if (Objects.nonNull(parameter)) {
+            ps.setString(i, parameter.toJSONString());
+        } else {
+            ps.setString(i, "{}");
+        }
     }
 
     @Override
     public JSONObject getNullableResult(ResultSet rs, String columnName)
             throws SQLException {
-        return JacksonUtils.parseObject(rs.getString(columnName));
+        return parseObject(rs.getString(columnName));
     }
 
     @Override
     public JSONObject getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        return JacksonUtils.parseObject(rs.getString(columnIndex));
+        return parseObject(rs.getString(columnIndex));
     }
 
     @Override
     public JSONObject getNullableResult(CallableStatement cs, int columnIndex)
             throws SQLException {
-        return JacksonUtils.parseObject(cs.getString(columnIndex));
+        return parseObject(cs.getString(columnIndex));
+    }
+
+    private JSONObject parseObject(String text) {
+        try {
+            return JSON.parseObject(text);
+        } catch (Exception ignored) {
+        }
+        return null;
     }
 
 }
